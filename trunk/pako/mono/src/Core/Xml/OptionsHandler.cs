a6fe66cc07bbc3d70ce31ebf69b2751513a55feb
bbodio@i.ua <bbodio@i.ua@dc63d190-0441-0410-ae29-ffca25385b93>
2008-07-21 13:54:07 +0000

    Please read the README, use the new configurator. Now you can set sqlite version on your machine by "sqlite" parameter. good luck

---
+++ trunk/pako/mono/src/Core/Xml/OptionsHandler.cs
@@ -0,0 +1,213 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Pako Jabber-bot. Bbodio's Lab.                                                *
+ * Copyright. All rights reserved © 2007-2008 by Klichuk Bogdan (Bbodio's Lab)   *
+ * Contact information is here: http://code.google.com/p/pako                    *
+ *                                                                               *
+ * Pako is under GNU GPL v3 license:                                             *
+ * YOU CAN SHARE THIS SOFTWARE WITH YOUR FRIEND, MAKE CHANGES, REDISTRIBUTE,     *
+ * CHANGE THE SOFTWARE TO SUIT YOUR NEEDS, THE GNU GENERAL PUBLIC LICENSE IS     *
+ * FREE, COPYLEFT LICENSE FOR SOFTWARE AND OTHER KINDS OF WORKS.                 *
+ *                                                                               *
+ * Visit http://www.gnu.org/licenses/gpl.html for more information about         *
+ * GNU General Public License v3 license                                         *
+ *                                                                               *
+ * Download source code: http://pako.googlecode.com/svn/trunk                    *
+ * See the general information here:                                             *
+ * http://code.google.com/p/pako.                                                *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using Core.Other;
+using Core.Kernel;
+using Core.Xml;
+using System;
+using System.Collections.Generic;
+using agsXMPP;
+using agsXMPP.Xml.Dom;
+
+namespace Core.Xml
+{
+	public class OptionsHandler : XMLContainer
+	{
+		
+		public OptionsHandler(string file)
+        {
+            if (!System.IO.File.Exists(file))
+            {
+                Document doc = new Document();
+                doc.LoadXml("<Options></Options>");
+                doc.Save(file);
+            }
+            Open(file, 10);
+            
+           @out.exe("option_hnd_created_or_opened");
+           string[] opts = new string[] { "global_censor", "censor_result", "amoderator", "akick", "avisitor", "aliases", "mode", "cmdaccess", "cleanup_unit" };
+           foreach (string opt in opts)
+           AddOption(opt); 
+            
+            
+        }
+        
+        
+
+        public string GetOption(string Name)
+        {
+        	lock (Document)
+		    {
+        	foreach (Element el in Document.RootElement.SelectElements("option"))
+        	{
+        		string name = el.GetAttribute("name");
+        		if (name == Name)
+        		{
+					return el.GetAttribute("value");
+				}
+			}
+			return null;
+		    }
+						
+		}
+		
+		public bool SetOption(string Name, string Value)
+		{
+			lock (Document)
+			{
+				foreach (Element el in Document.RootElement.SelectElements("option"))
+        	    {
+        		  string name = el.GetAttribute("name");
+        		  if (name == Name)
+        		  {
+				     Value = Value.ToLower();
+				     string[] opts = GetPossibleValues(Name);
+				     foreach (string opt in opts)
+				     {
+					    if (opt == Value || opt == "*")
+					    { el.SetAttribute("value", Value); Save(); return true; }
+				     }
+				     return false;
+			      }
+			   }
+			   return false;
+				
+			}
+		}
+		
+		public List<string> GetOptions()
+		{
+			lock (Document)
+			{
+				List<string> data = new List<string>();
+				foreach (Element el in Document.RootElement.SelectElements("option"))
+				data.Add(el.GetAttribute("name"));
+				return data;
+			}
+		}
+		
+		public string[] GetPossibleValues(string Name)
+		{
+			lock (Document)
+			{
+		    foreach (Element el in Document.RootElement.SelectElements("option"))
+        	{
+        		string name = el.GetAttribute("name");
+        		if (name == Name)
+        		{
+        			return el.GetAttribute("possible").Split(new char[] {'|'}, StringSplitOptions.RemoveEmptyEntries);
+				}
+			}
+			return null;
+		    }
+			
+		}
+        
+        
+        public bool AddOption(string Name)
+        {
+           
+        	lock (Document)
+        	{
+        		@out.exe("option_start");
+        	 if (GetOption(Name) != null)
+        	 return false;
+        	 @out.exe("option_adding");
+        	 switch (Name)
+        	 {
+                 case "amoderator":
+                 case "akick":
+                 case "cmdaccess":
+                 case "avisitor":
+                 case "aliases":
+        	     case "global_censor":
+        	         Document.RootElement.AddTag("option");
+                    foreach (Element el in Document.RootElement.SelectElements("option"))
+                    {
+                        if (!el.HasAttribute("name"))
+                        {
+                            el.SetAttribute("name", Name);
+                            el.SetAttribute("value", "+");
+                            el.SetAttribute("possible","+|-");
+                            Save();
+                            return true;
+                        }
+                    }
+                    break;
+                     
+                
+                case "censor_result":
+                   Document.RootElement.AddTag("option");
+                    foreach (Element el in Document.RootElement.SelectElements("option"))
+                    {
+                        if (!el.HasAttribute("name"))
+                        {
+                            el.SetAttribute("name", Name);
+                            el.SetAttribute("value", "kick");
+                            el.SetAttribute("possible","kick|devoice|nothing|warn|ban");
+                            Save();
+                            return true;
+                        }
+                    }
+                    break;
+
+                case "cleanup_unit":
+                    Document.RootElement.AddTag("option");
+                    foreach (Element el in Document.RootElement.SelectElements("option"))
+                    {
+                        if (!el.HasAttribute("name"))
+                        {
+                            el.SetAttribute("name", Name);
+                            el.SetAttribute("value", "~");
+                            el.SetAttribute("possible", "*|null|empty");
+                            Save();
+                            return true;
+                        }
+                    }
+                    break;
+
+                case "mode":
+                   Document.RootElement.AddTag("option");
+                    foreach (Element el in Document.RootElement.SelectElements("option"))
+                    {
+                        if (!el.HasAttribute("name"))
+                        {
+                            el.SetAttribute("name", Name);
+                            el.SetAttribute("value", "full");
+                            el.SetAttribute("possible","full|private|groupchat");
+                            Save();
+                            return true;
+                        }
+                    }
+                    break;
+                default: return false;
+                    
+			 }
+			 return true;
+		    }
+		    
+		 }
+		 
+		 	 
+		
+        
+        
+	}
+	
+}
+
---
+++ trunk/pako/mono/src/Plugins/Muc/Main.cs
@@ -0,0 +1,69 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Pako Jabber-bot. Bbodio's Lab.                                                *
+ * Copyright. All rights reserved © 2007-2008 by Klichuk Bogdan (Bbodio's Lab)   *
+ * Contact information is here: http://code.google.com/p/pako                    *
+ *                                                                               *
+ * Pako is under GNU GPL v3 license:                                             *
+ * YOU CAN SHARE THIS SOFTWARE WITH YOUR FRIEND, MAKE CHANGES, REDISTRIBUTE,     *
+ * CHANGE THE SOFTWARE TO SUIT YOUR NEEDS, THE GNU GENERAL PUBLIC LICENSE IS     *
+ * FREE, COPYLEFT LICENSE FOR SOFTWARE AND OTHER KINDS OF WORKS.                 *
+ *                                                                               *
+ * Visit http://www.gnu.org/licenses/gpl.html for more information about         *
+ * GNU General Public License v3 license                                         *
+ *                                                                               *
+ * Download source code: http://pako.googlecode.com/svn/trunk                    *
+ * See the general information here:                                             *
+ * http://code.google.com/p/pako.                                                *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Plugins;
+using Core.Kernel;
+using Core.Conference;
+using agsXMPP;
+using agsXMPP.protocol.client;
+
+namespace Plugin
+{
+ 
+    public class Main : IPlugin
+    {
+
+        public string File
+        {
+            get
+            {
+                return "Muc.dll";
+            }
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return "Muc";
+            }
+        }
+
+        public string Comment
+        {
+            get
+            {
+                return "Can serve muc users" ;
+            }
+        }
+
+        public void PerformAction(IPluginData d)
+        {
+           
+            MucHandler ph = new MucHandler(d.r, Name);
+
+        }
+    }
+
+
+
+}
