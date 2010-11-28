09f4d88d34ca83ee8688f746d36fac61338a06da
alexey.cv <alexey.cv@dc63d190-0441-0410-ae29-ffca25385b93>
2010-11-28 14:17:11 +0000

    Update DataController class

--- trunk/pako/mono/src/Core/API/Data/DataController.cs
+++ trunk/pako/mono/src/Core/API/Data/DataController.cs
@@ -81,6 +81,16 @@ namespace Core.API.Data
 			SQLiteConn.Close();
 		}
 		
+		/// <summary>
+		/// Execute database query that's don't return any result. 
+		/// Return value : number of rows affected.
+		/// </summary>
+		/// <param name="command">
+		/// A <see cref="String"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="System.Int32"/>
+		/// </returns>
 		public int ExecuteNonQuery(String command)
 		{
 			try
@@ -95,6 +105,16 @@ namespace Core.API.Data
 			}
 		}
 		
+		/// <summary>
+		/// Execute DataReader. 
+		/// Returns DataReader.
+		/// </summary>
+		/// <param name="command">
+		/// A <see cref="String"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="SqliteDataReader"/>
+		/// </returns>
 		public SqliteDataReader ExecuteReader(String command)
 		{
 			try
@@ -109,11 +129,74 @@ namespace Core.API.Data
 			}
 		}
 		
-		public int ExecuteDANonQuery(String command, DataTable table)
+		/// <summary>
+		/// Load query result to a DataTable object
+		/// Return value : DataTable.
+		/// </summary>
+		/// <param name="command">
+		/// A <see cref="String"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="DataTable"/>
+		/// </returns>
+		public DataTable ExecuteDALoad(String command)
 		{
 			try
 			{
 				SqliteDataAdapter _da = new SqliteDataAdapter(@""+command, SQLiteConn);
+				SqliteCommandBuilder _cb = new SqliteCommandBuilder();
+				_cb.DataAdapter = _da;
+				
+				DataTable _tmpTable = new DataTable();
+				_da.Fill(_tmpTable);
+				
+				if (_tmpTable.Rows.Count > 0)
+				{
+					return _tmpTable;
+				}
+				else
+				{
+					return null;
+				}
+			}
+			catch (Exception exx)
+			{
+				return null;
+			}
+		}
+		
+		/// <summary>
+		/// Execute saving data over DataAdapter
+		/// </summary>
+		/// <param name="command">
+		/// A <see cref="String"/>
+		/// </param>
+		/// <param name="row">
+		/// A <see cref="DataRow"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="System.Int32"/>
+		/// </returns>
+		public int ExecuteDASave(String command, DataRow row)
+		{
+			try
+			{
+				SqliteDataAdapter _da = new SqliteDataAdapter(@""+command, SQLiteConn);
+				SqliteCommandBuilder _cb = new SqliteCommandBuilder();
+				_cb.DataAdapter = _da;
+				
+				DataTable _tmpTable = new DataTable();
+				_da.Fill(_tmpTable);
+				
+				if (_tmpTable.Rows.Count == 0)
+				{
+					DataRow _dr = _tmpTable.NewRow();
+					_tmpTable.Rows.Add(_dr);
+				}
+				
+				_tmpTable.Rows[0].ItemArray = row.ItemArray;
+				
+				_da.Update(_tmpTable);
 				
 				return 0;
 			}
