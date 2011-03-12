d77c4128a89bb87bb9a070373c898fdbe3d0a180
alexey.cv@gmail.com <alexey.cv@gmail.com@dc63d190-0441-0410-ae29-ffca25385b93>
2011-03-12 15:37:12 +0000

    scheduler : add classess

---
+++ trunk/pako/mono/src/Plugins/Scheduler/Scheduler.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Plugins;
+using Core.Kernel;
+using Core.Conference;
+using agsXMPP;
+using agsXMPP.protocol.client;
+
+namespace Plugin
+{
+ 
+    public class Scheduler		
+    {
+		
+	}
+}
---
+++ trunk/pako/mono/src/Plugins/Scheduler/SchedulerTask.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Plugins;
+using Core.Kernel;
+using Core.Conference;
+using agsXMPP;
+using agsXMPP.protocol.client;
+
+namespace Plugin
+{
+ 
+    public class SchedulerTask
+    {
+		
+	}
+}
+ 
