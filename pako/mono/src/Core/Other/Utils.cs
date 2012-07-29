584151cca977bbdaf6d8c4549babc4d5da95244f
alexey.cv@gmail.com <alexey.cv@gmail.com@dc63d190-0441-0410-ae29-ffca25385b93>
2012-07-29 15:01:40 +0000

    update web weather command

--- pako/mono/src/Core/Other/Utils.cs
+++ pako/mono/src/Core/Other/Utils.cs
@@ -621,7 +621,7 @@ namespace Core.Other
 
 
 		private static string Version {
-			get { return "12.07.28 (http://pako.googlecode.com developers team)"; }
+			get { return "12.07.29 (http://pako.googlecode.com developers team)"; }
 		}
 		
 		
